39272a495ac7a12c40adb8588e828a69ede70c2c
itpark <a.m.smyslov@yandex.ru>
2023-04-03 22:06:11 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import {Modal, Table} from "antd";
+import {DeleteOutlined, EditOutlined} from "@ant-design/icons";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    let [dataSource, setDataSource] = useState([]);
+
+    const loadTodos = () => {
+        axios.get("http://localhost:8080/todos/getAll")
+            .then(response => {
+                setDataSource(response.data);
+            });
+    }
+
+    const deleteTodoById = (id) => {
+        axios.delete(`http://localhost:8080/todos/deleteById/${id}`)
+            .then(response => {
+                loadTodos();
+            });
+    }
+
+
+    useEffect(() => {
+        loadTodos();
+    }, []);
+
+    const columns = [
+        // {
+        //     key: "1",
+        //     title: "ID",
+        //     dataIndex: "id",
+        // },
+        {
+            key: "2",
+            title: "Date",
+            dataIndex: "date",
+        },
+        {
+            key: "3",
+            title: "Description",
+            dataIndex: "description",
+        },
+        {
+            key: "5",
+            title: "Actions",
+            render: (record) => {
+                return (
+                    <div>
+                        <EditOutlined
+                            onClick={() => {
+                                //onEditStudent(record);
+                            }}
+                        />
+                        <DeleteOutlined
+                            onClick={() => {
+                                onDeleteStudent(record);
+                            }}
+                            style={{color: "red", marginLeft: 12}}
+                        />
+                    </div>
+                );
+            },
+        }
+    ];
+
+    const onDeleteStudent = (record) => {
+        Modal.confirm({
+            title: "Точно удалить студента?",
+            okText: "Да",
+            okType: "danger",
+            onOk: () => {
+                deleteTodoById(record.id);
+                //setDataSource(dataSource.filter(student => student.id !== record.id));
+            },
+        });
+    };
+
+
+    return (
+        <div>
+            <Table dataSource={dataSource} columns={columns}/>
+        </div>
+    );
+};
 
 export default App;
